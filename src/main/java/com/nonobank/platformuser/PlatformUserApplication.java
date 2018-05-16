7430b0a3bad74806f188883d7647539ce526373c
tangrubei <tangrubei@nonobank.com>
2018-05-16 17:01:03 +0800

    修改session时长

--- src/main/java/com/nonobank/platformuser/PlatformUserApplication.java
+++ src/main/java/com/nonobank/platformuser/PlatformUserApplication.java
@@ -3,11 +3,13 @@ package com.nonobank.platformuser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 
 @SpringBootApplication
 @EnableAutoConfiguration
 //@EnableRedisHttpSession
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400*30)
 public class PlatformUserApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PlatformUserApplication.class, args);
