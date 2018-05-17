aa452bf40d8b67ab5ffefd82a4a64e549f230c31
ted <ted@teddeMacBook-Pro.local>
2018-05-17 10:39:10 +0800

    修改查询用户的sql

--- src/main/java/com/nonobank/platformuser/repository/mysqlRepository/UserRepository.java
+++ src/main/java/com/nonobank/platformuser/repository/mysqlRepository/UserRepository.java
@@ -18,7 +18,7 @@ public interface UserRepository extends JpaRepository<User,Integer> {
 //    @Query(value="select u.id, u.username,u.nickname, ur.role_id from user u left join user_roles ur on u.id=ur.user_id ", nativeQuery = true)
 //    @Query("select new com.nonobank.platformuser.entity.mysqlEntity.UserFront(id,username,roleId) from Role where id in (select roleId from UserRoles where userId=:userId) and optstatus <> 2")
 
-    @Query(value="select u.id, u.username, u.nickname,  ur.role_id, r.role_name from user u, user_roles ur, role r where u.id=ur.user_id and ur.role_id=r.id", nativeQuery = true)
+    @Query(value="select u.id, u.username, u.nickname,  t.role_id, t.role_name from user u left join (select ur.user_id, ur.role_id, r.role_name from user_roles ur, role r where ur.role_id=r.id ) t on u.id=t.user_id", nativeQuery = true)
     List<Object[]> findAllUsers();
 
 }
